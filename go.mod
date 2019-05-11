6e924f5628a307d641f92c30f71fac595ab1424f
HectorGabucio <hectordorito5@gmail.com>
2019-05-11 19:12:57 +0200

    select tasks

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module todoapp-api
 
 go 1.12
 
-require github.com/gorilla/mux v1.7.1
+require (
+	github.com/gorilla/mux v1.7.1
+	github.com/jinzhu/gorm v1.9.8
+	github.com/lib/pq v1.1.1
+)
--- main.go
+++ main.go
@@ -1,23 +1,75 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/mux"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-func indexHandler(rw http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(rw, "Hello, Heroku!")
+//Task comment
+type Task struct {
+	gorm.Model
+	Name string `json:"name"`
 }
 
+// respondJSON makes the response with payload as json format
+func respondJSON(w http.ResponseWriter, status int, payload interface{}) {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write([]byte(response))
+}
+
+// respondError makes the error response with payload as json format
+func respondError(w http.ResponseWriter, code int, message string) {
+	respondJSON(w, code, map[string]string{"error": message})
+}
+
+func allTasks(rw http.ResponseWriter, req *http.Request) {
+
+	db, err := gorm.Open("postgres", os.Getenv("DATABASE_URL"))
+	defer db.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tasks := []Task{}
+	db.Find(&tasks)
+	respondJSON(rw, http.StatusOK, tasks)
+}
+
+func DBMigrate() {
+	db, err := gorm.Open("postgres", os.Getenv("DATABASE_URL"))
+	defer db.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//migrations
+	db.AutoMigrate(&Task{})
+}
+
+//http://www.golangprograms.com/golang-restful-api-using-grom-and-gorilla-mux.html
 func main() {
 	// Heroku supplies your port via environment variable
 	port := os.Getenv("PORT")
 
+	DBMigrate()
+
 	r := mux.NewRouter()
-	r.HandleFunc("/", indexHandler)
+
+	r.HandleFunc("/tasks", allTasks)
+
 	log.Fatal(http.ListenAndServe(":"+port, r))
 }
